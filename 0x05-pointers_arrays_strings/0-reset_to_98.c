ec9f5606950f9faed7ffc3fd857f29d3bc6e6148
Eman <relelrify@gmail.com>
2023-05-03 11:18:07 -0700

    change to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,9 @@
 void reset_to_98(int *n)
 {
 /**
- * reset_to_98 - check the code
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
  *
  * Return: Always 0
  */
 	*n = 98;
 }
-
