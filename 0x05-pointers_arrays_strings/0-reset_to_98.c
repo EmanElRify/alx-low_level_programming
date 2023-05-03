f2659aba92c71150943b10f0ceab415e27f0ba0b
Eman <relelrify@gmail.com>
2023-05-03 11:13:00 -0700

    change to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-/**
- * rest_to_98 
- *takes a pointer to an int as parameter and updates the value it points to to 98.
- * Return: Always 0.
- */
+/*
+ * Return: Always 0
+ * */
 void reset_to_98(int *n)
 {
 	*n = 98;
