eda8a8cabb01d5c5f76a02591746b93a0f3c5b11
Eman <relelrify@gmail.com>
2023-05-08 02:26:05 -0700

    putchar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	char s[] = "and that piece of art is useful/" - Dora Korpar, 2015-10-19";
-	putchar(s);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
