7d7ecfba8844ab38a176b0c3e57886811e16c7f3
Eman <relelrify@gmail.com>
2023-05-08 18:20:12 -0700

    print upper and lower alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' || c != 'e')
+		{
+			putchat(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
