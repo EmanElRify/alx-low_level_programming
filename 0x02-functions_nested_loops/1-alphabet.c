71f2682bd26cb3223b3153bb2084a22887242a1e
Eman <relelrify@gmail.com>
2023-05-09 16:57:43 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - a function to print alphabets in a lowercase
+ *
+ * Return: No return
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * main - source code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_print_alphabet();
+	return (0);
+}
