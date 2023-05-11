ddfb881f0ac6328adc8974e0ac61fcb4702696ec
Eman <relelrify@gmail.com>
2023-05-11 05:38:38 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,7 +17,7 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			product = i * j;
-			if (product > 9)
+			if ((product > 9) || ((i * (j + 1)) > 9))
 			{
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
