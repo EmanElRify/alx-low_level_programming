c2d05170dac6ab877c7a2f7d2d66514ef0138fb9
Eman <relelrify@gmail.com>
2023-05-11 15:59:20 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * *s: a string to be printed
+ * Return: no return value
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(*s);
+	}
+	_putchar(*s + 1);
+}
