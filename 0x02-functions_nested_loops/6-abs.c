51786473a2131150ba4fe4078c1814e312c46fd7
Eman <relelrify@gmail.com>
2023-05-11 03:13:58 -0700

    get absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs: computes the absolute value of an integer
+ * @n: integer to get its absolute value
+ * Return: Absolute value
+ */
+int _abs(int n)
+{
+	int abs_;
+
+	if (n >= 0)
+	{
+		abs_ = n;
+		return (abs_);
+	}
+	else
+	{
+		abs_ = n * (-1);
+		return (abs_);
+	}
+}
