a70237843fef3e834356cc542158ed82f07d989e
Eman <relelrify@gmail.com>
2023-05-11 20:46:34 -0700

    test

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raise a value to a power
+ * @x: the raised value
+ * @y: the power value
+ * Return: the result of x powered to y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+	{
+		return (x);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
