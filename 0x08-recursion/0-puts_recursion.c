b237679f1d4438cb2582d9e1e62cd6195c19e5a9
Eman <relelrify@gmail.com>
2023-05-11 19:45:45 -0700

    print string with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,11 +2,18 @@
 
 /**
  * _puts_recursion - prints a string, followed by a new line
- * @*s: a string to be printed
+ * @s: a string to be printed
  * Return: no return value
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
