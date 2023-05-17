b8e26e21dfd46fc34b8ce89113e987ebf202a342
Eman <relelrify@gmail.com>
2023-05-17 14:30:27 -0700

    static

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,1 +1,13 @@
-char *_strcpy(char *dest, char *src);
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - 
+ * @dest:
+ * @scr:
+ * Return: Always 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (0);
+}
