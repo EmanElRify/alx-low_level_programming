940f860a92ab6eb4a100c53f81531edf6e985230
Eman <relelrify@gmail.com>
2023-10-02 02:06:52 -0700

    reversed string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,13 +5,13 @@
  * @s: a string to be reversed
  * Return: None
  */
-void print_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	print_rev_recursion(s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
