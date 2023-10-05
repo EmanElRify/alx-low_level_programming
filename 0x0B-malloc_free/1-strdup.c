43baeb9365dc01fcf90a5ebe02b332f52297b831
Eman <relelrify@gmail.com>
2023-10-04 19:55:47 -0700

    newly allocated copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup -  returns a pointer to a newly allocated space in memory
+ * @str: the string to be copied
+ * Return:  a pointer to a newly allocated space (Success), Null (Error)
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	unsigned int i, len;
+
+	i = 0;
+	len = 0;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[len])
+	{
+		len++;
+	}
+	p = malloc(sizeof(char) * (len + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	while((p[i] = str[i]) != '\0')
+	{
+		i++;
+	}
+	return (p);
+}
