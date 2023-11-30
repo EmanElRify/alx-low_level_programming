de4cb0962209a5fa88c3f5c7b9a9441c8f53241e
Eman <relelrify@gmail.com>
2023-11-30 14:22:35 -0800

    sum all parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,11 +7,14 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-		return (0);
 	va_list args;
 	unsigned int i;
 	int sum = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
