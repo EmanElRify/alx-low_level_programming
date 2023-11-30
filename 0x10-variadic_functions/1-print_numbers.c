d75598a3bf6af133620e2658369efd3832315273
Eman <relelrify@gmail.com>
2023-11-30 14:49:35 -0800

    print numbers with separators

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <string.h>
+
+/**
+ * print_numbers - a function that prints the numbers followed by a newline
+ * @separator: the string to be printed between numbers
+ * @n: number of integers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i == n - 1)
+			break;
+		printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+	return;
+}
